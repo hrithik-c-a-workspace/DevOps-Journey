19fcbdf408e9e1bb13bd8b9d94265547b4184d48
Hrithik C A <hrithik-c-a@zohomail.in>
2025-05-29 11:42:01 +0530

    Completed exercise 1

---
+++ Python-for-DevOps/Exercise-1/e1.py
@@ -0,0 +1,29 @@
+'''
+    Write a Python function that takes a name as an argument and prints that name.
+'''
+import re
+
+# Reusable functions
+def input_validator(value = ''):
+    if len(value) == 0:
+        print('Input cannot be empty.')
+        return False
+    
+    pattern = r"^[A-Z][a-zA-Z\s\-]*$"
+    if re.fullmatch(pattern=pattern, string=value):
+        return True
+    
+    print('Please enter a valid Input.')
+    return False
+
+def get_input():
+        name = str(input('Enter a name: '))
+        name.strip()
+        if input_validator(value=name):
+                return name
+        return None
+
+entered_name = get_input()
+
+if entered_name != None:
+    print(f'The name you have entered is: {entered_name}')
